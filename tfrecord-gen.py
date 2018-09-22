eb2a47fbd498d4efd551ca37ddd21feb61248268
Jordan Chadwick <jordan@wickwick.org>
2018-09-22 15:34:21 -0400

    added tfrecord-gen.py

---
+++ tfrecord-gen.py
@@ -0,0 +1,16 @@
+#
+# tfrecord-gen.py
+#
+# Converts various datasets to
+# tfrecords.
+#
+
+from __future__ import print_function
+
+
+def main():
+    # parse arguments
+
+
+if __name__ == "__main__":
+    main()
