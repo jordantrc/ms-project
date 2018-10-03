4a72104edfd56d1c4bb8e1b6414ccf45471a9320
Jordan Chadwick <jordan@wickwick.org>
2018-10-03 11:22:03 -0400

    working video to numpy

--- cv2-testing/video-to-numpy.py
+++ cv2-testing/video-to-numpy.py
@@ -3,9 +3,12 @@
 from __future__ import print_function
 
 import cv2
+import numpy as np
 import random
 import sys
 
+
+np.set_printoptions(threshold='nan')
 
 samples = 5
 sample_length = 2
@@ -36,45 +39,61 @@ print("fps = %d" % (fps))
 print("length = %f" % (frame_count / fps))
 
 video_seconds = list(range(0, int(frame_count / fps)))
-start_times = []
+sample_times = []
 oversample = False
 
 if len(video_seconds[::sample_length]) > samples:
-    start_times = random.sample(video_seconds[::sample_length], samples)
-    start_times.sort()
+    sample_times = random.sample(video_seconds[::sample_length], samples)
+    add_times = []
+    for s in sample_times:
+        for i in range(1, sample_length):
+            add_times.append(s + i)
+    sample_times.extend(add_times)
+    sample_times.sort()
 else:
     # oversampling is needed
     oversample = True
     index = 0
-    while len(start_times) < samples:
-        start_times.append(video_seconds[index])
+    while len(sample_times) < (samples * sample_length):
+        sample_times.append(video_seconds[index])
         index += 1
         if index == len(video_seconds):
             index = 0
 
-print("start_times = %s" % (start_times))
+print("sample_times = %s" % (sample_times))
 success, image = cap.read()
+
+buf = np.empty((int(fps * sample_length * samples), height, width, 3), np.dtype('uint8'))
 
 in_second = 0
 count = 0
+sequence = 0
 while success:
-    if in_second in start_times:
+    if in_second in sample_times:
         if oversample:
-            num_samples = start_times.count(in_second)
-            indices = [i for i, x in enumerate(start_times) if x == in_second]
+            num_samples = sample_times.count(in_second)
+            indices = [i for i, x in enumerate(sample_times) if x == in_second]
         else:
             num_samples = 1
 
         if num_samples == 1:
-            cv2.imwrite("./frames/frame%d-%d.jpg" % (in_second, count), image)
+            # cv2.imwrite("./frames/frame%d-%d.jpg" % (sequence, count), image)
+            buf[sequence] = image
+            sequence += 1
         else:
             for s in range(num_samples):
                 index = indices[s]
-                frame_index = in_second + index + s
+                sequence = int((fps * index) + (count % fps))
+                buf[sequence] = image
+                # print("index = %s sequence = %s" % (index, sequence))
                 # print("frame index = %s" % frame_index)
-                cv2.imwrite("./frames/frame%d-%d.jpg" % (frame_index, count), image)
+                cv2.imwrite("./frames/frame%d-%d.jpg" % (sequence, count), image)
 
     success, image = cap.read()
     # print('read new frame: ', success)
     count += 1
     in_second = int(count / fps)
+
+print("np buffer shape, sample data:")
+print(buf.shape)
+print(buf[0])
