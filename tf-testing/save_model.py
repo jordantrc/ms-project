a53171543924ba428f73fb96850df3c3d2ac2968
Jordan Chadwick <jordan.chadwick@gmail.com>
2018-10-30 13:57:24 -0400

    test files

--- tf-testing/save_model.py
+++ tf-testing/save_model.py
@@ -1,22 +1,11 @@
 # model save
 
 import tensorflow as tf
+import model
+
 
 def save_model():
-    xx = [1, 2, 3]
-    y = [1, 2, 3]
-
-    x = tf.placeholder(tf.float32)
-
-    w = tf.Variable(tf.random_normal([1], -1, 1))
-    b = tf.Variable(tf.random_normal([1], -1, 1))
-
-    hypothesis = w * x + b
-
-    cost = tf.reduce_mean((hypothesis - y) ** 2)
-
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1)
-    train = optimizer.minimize(loss=cost)
+    model.create_graph()
 
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
