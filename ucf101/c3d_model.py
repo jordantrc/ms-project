a60cd0b5fcc1432c7714d770646b25e27cbdbc22
Jordan Chadwick <jordan.chadwick@gmail.com>
2018-11-01 14:36:44 -0400

    refactor c3d_model to be an object

--- ucf101/c3d_model.py
+++ ucf101/c3d_model.py
@@ -111,35 +111,35 @@ class C3DModel():
         print("_X = %s" % _X)
 
         # Convolution layer
-        conv1 = conv3d('conv1', _X, _weights['wc1'], _biases['bc1'])
+        conv1 = self.conv3d('conv1', _X, _weights['wc1'], _biases['bc1'])
         conv1 = tf.nn.relu(conv1, 'relu1')
-        pool1 = max_pool('pool1', conv1, k=1)
+        pool1 = self.max_pool('pool1', conv1, k=1)
 
         # Convolution Layer
-        conv2 = conv3d('conv2', pool1, _weights['wc2'], _biases['bc2'])
+        conv2 = self.conv3d('conv2', pool1, _weights['wc2'], _biases['bc2'])
         conv2 = tf.nn.relu(conv2, 'relu2')
-        pool2 = max_pool('pool2', conv2, k=2)
+        pool2 = self.max_pool('pool2', conv2, k=2)
 
         # Convolution Layer
-        conv3 = conv3d('conv3a', pool2, _weights['wc3a'], _biases['bc3a'])
+        conv3 = self.conv3d('conv3a', pool2, _weights['wc3a'], _biases['bc3a'])
         conv3 = tf.nn.relu(conv3, 'relu3a')
-        conv3 = conv3d('conv3b', conv3, _weights['wc3b'], _biases['bc3b'])
+        conv3 = self.conv3d('conv3b', conv3, _weights['wc3b'], _biases['bc3b'])
         conv3 = tf.nn.relu(conv3, 'relu3b')
-        pool3 = max_pool('pool3', conv3, k=2)
+        pool3 = self.max_pool('pool3', conv3, k=2)
 
         # Convolution Layer
-        conv4 = conv3d('conv4a', pool3, _weights['wc4a'], _biases['bc4a'])
+        conv4 = self.conv3d('conv4a', pool3, _weights['wc4a'], _biases['bc4a'])
         conv4 = tf.nn.relu(conv4, 'relu4a')
-        conv4 = conv3d('conv4b', conv4, _weights['wc4b'], _biases['bc4b'])
+        conv4 = self.conv3d('conv4b', conv4, _weights['wc4b'], _biases['bc4b'])
         conv4 = tf.nn.relu(conv4, 'relu4b')
-        pool4 = max_pool('pool4', conv4, k=2)
+        pool4 = self.max_pool('pool4', conv4, k=2)
 
         # Convolution Layer
-        conv5 = conv3d('conv5a', pool4, _weights['wc5a'], _biases['bc5a'])
+        conv5 = self.conv3d('conv5a', pool4, _weights['wc5a'], _biases['bc5a'])
         conv5 = tf.nn.relu(conv5, 'relu5a')
-        conv5 = conv3d('conv5b', conv5, _weights['wc5b'], _biases['bc5b'])
+        conv5 = self.conv3d('conv5b', conv5, _weights['wc5b'], _biases['bc5b'])
         conv5 = tf.nn.relu(conv5, 'relu5b')
-        pool5 = max_pool('pool5', conv5, k=2)
+        pool5 = self.max_pool('pool5', conv5, k=2)
 
         # Fully connected layer
         pool5 = tf.transpose(pool5, perm=[0, 1, 4, 2, 3])
