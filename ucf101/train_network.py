6b9c2449998e42963f1d3cd1979e42fcadc253d0
Jordan Chadwick <jordan.chadwick@gmail.com>
2018-11-02 16:12:50 -0400

    balanced classes for subset of train data

--- ucf101/train_network.py
+++ ucf101/train_network.py
@@ -80,7 +80,7 @@ def balance_classes(files):
         if smallest_class is None:
             smallest_class_count = v
             smallest_class = k
-        elif v > smalles_class_count:
+        elif v > smallest_class_count:
             smallest_class_count = v
             smallest_class = k
 
