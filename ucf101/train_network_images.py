a25b907614cd0bc3ebe6bb11d482d068e32ae0f8
Jordan Chadwick <jordan.chadwick@gmail.com>
2018-11-18 10:55:26 -0500

    fix normalize

--- ucf101/train_network_images.py
+++ ucf101/train_network_images.py
@@ -211,7 +211,7 @@ def get_image_batch(filename, batch_size, frames_per_clip, num_classes, offset=-
         if len(frames) > 0:
             images = []
             for j in xrange(len(frames)):
-                img = Image.fromarray(frames[j].astype(np.uint8))
+                img = np.array(Image.fromarray(frames[j].astype(np.uint8)))
                 # if img.width > img.height:
                 #     scale = float(crop_size) / float(img.height)
                 #     img = np.array(cv2.resize(np.array(img), (int(img.width * scale + 1), crop_size))).astype(np.float32)
