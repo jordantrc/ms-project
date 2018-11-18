692eb4bb0c9264a14d34c719f46794a5d7489175
Jordan Chadwick <jordan.chadwick@gmail.com>
2018-11-17 20:59:03 -0500

    update to work with image folders

--- ucf101/train_network_images.py
+++ ucf101/train_network_images.py
@@ -198,7 +198,9 @@ def get_image_batch(filename, batch_size, frames_per_clip, num_classes, offset=-
         if len(frames) > 0:
             for j in xrange(len(frames)):
                 img = Image.fromarray(frames[j].astype(np.uint8))
+                print("[get_image_batch] img pre-crop = %s" % (np.shape(img)))
                 img = np.array(cv2.resize(np.array(img), (crop_size, crop_size))).astype(np.float32)
+                print("[get_image_batch] img post-crop = %s" % (np.shape(img)))
                 # if img.width > img.height:
                 #     scale = float(crop_size) / float(img.height)
                 #     img = np.array(cv2.resize(np.array(img), (int(img.width * scale + 1), crop_size))).astype(np.float32)
@@ -206,7 +208,9 @@ def get_image_batch(filename, batch_size, frames_per_clip, num_classes, offset=-
                 #     scale = float(crop_size) / float(img.width)
                 #     img = np.array(cv2.resize(np.array(img), (crop_size, int(img.height * scale + 1)))).astype(np.float32)
                 img = tf.image.per_image_standardization(img)
+                print("[get_image_batch] img post-standard. = %s" % (np.shape(img)))
                 img = tf.reshape(img, [1, crop_size, crop_size, 3])
+                print("[get_image_batch] img post-reshape = %s" % (np.shape(img)))
                 data.append(img)
                 print("[get_image_batch] data = %s" % (np.shape(data)))
             labels.append(label)
