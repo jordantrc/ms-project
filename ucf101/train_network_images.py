690225246d403418415e7ffb824d17da14af9f78
Jordan Chadwick <jordan.chadwick@gmail.com>
2018-11-17 20:20:35 -0500

    update to work with image folders

--- ucf101/train_network_images.py
+++ ucf101/train_network_images.py
@@ -215,7 +215,6 @@ def get_image_batch(filename, batch_size, frames_per_clip, num_classes, offset=-
             labels.append(labels[j])
             j += 1
 
-    data = tf.stack(data)
     np_arr_label = np.array(labels).astype(np.int64)
     labels_one_hot = tf.one_hot(np_arr_label, depth=num_classes)
 
