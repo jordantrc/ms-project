41f813d3d20e149824623f74718036c48bce09bc
Jordan Chadwick <jordan.chadwick@gmail.com>
2018-11-29 20:45:30 -0500

    incorporating IAD gen

---
+++ ucf101/fix_test_list.py
@@ -0,0 +1,18 @@
+
+import sys
+from tfrecord_gen import CLASS_INDEX_FILE, get_class_list
+
+classes = get_class_list(CLASS_INDEX_FILE)
+
+filename = sys.argv[1]
+
+with open(filename) as fd:
+	text = fd.read()
+	lines = text.split('\n')
+
+with open(filename, 'w') as fd:
+	for l in lines:
+		directory, file = l.split('/')
+		label = file.split('_')[1]
+		int_label = classes.index(label)
+		fd.write(directory + "/" + file + " " + int_label + "\n")
