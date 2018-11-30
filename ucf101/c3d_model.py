05c4fe15b7768dc2064039bc3022994652dd7e30
Jordan Chadwick <jordan.chadwick@gmail.com>
2018-11-29 19:34:59 -0500

    incorporating IAD gen

--- ucf101/c3d_model.py
+++ ucf101/c3d_model.py
@@ -327,7 +327,7 @@ def get_frame_data(filename, num_frames_per_clip=16):
   height = features['height']
   width = features['width']
   channels = features['channels']
-  frames = tf.reshape(frame_stack, tf.stack([num_frames, height, width, channels]))
+  frames = tf.parallel_stack(frame_stack, [num_frames, height, width, channels])
 
   # sample num_frames_per_clip frames from the frame stack
   if num_frames == num_frames_per_clip:
