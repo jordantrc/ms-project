5512c8699a3eacd164f1af614114a1bbba616387
Jordan Chadwick <jordan.chadwick@gmail.com>
2018-12-03 09:19:10 -0500

    IAD gen

--- ucf101/iad_gen_ucf101.py
+++ ucf101/iad_gen_ucf101.py
@@ -86,7 +86,6 @@ def convert_to_IAD_input(directory, layers, sample_names, labels, compression_me
         video_name = os.path.join(directory, s + ".tfrecord")
 
         # save a random layer as an image
-        layer_to_test = random.randint(0, num_layers - 1)
         img_name = os.path.join(directory, s + "_" + str(layer_to_test) + ".jpg")
 
         s_index = i * num_layers
@@ -106,14 +105,16 @@ def convert_to_IAD_input(directory, layers, sample_names, labels, compression_me
         writer.write(ex.SerializeToString())
 
         # generate the image
-        print("write test to: ", img_name)
-        print("single layer type = %s, shape = %s" % (type(thresholded_data[layer_to_test]), str(thresholded_data[layer_to_test].shape)))
-        pixels = np.squeeze(thresholded_data[layer_to_test], axis=2)
-        rescaled = (255.0 / pixels.max() * (pixels - pixels.min())).astype(np.uint8)
-        img = Image.fromarray(rescaled)
-        img = img.convert("L")
-        img = img.resize((img.width * 10, img.heigt))
-        img.save(img_name, quality=95)
+        for i, d in enumerate(thresholded_data):
+            img_name = os.path.join(directory, s + "_" + str(i) + ".jpg")
+            print("write test image to: ", img_name)
+            #print("single layer type = %s, shape = %s" % (type(d), str(thresholded_data[layer_to_test].shape)))
+            pixels = np.squeeze(d, axis=2)
+            rescaled = (255.0 / pixels.max() * (pixels - pixels.min())).astype(np.uint8)
+            img = Image.fromarray(rescaled)
+            img = img.convert("L")
+            img = img.resize((img.width * 10, img.heigt))
+            img.save(img_name, quality=95)
 
 
 def conv3d(name, l_input, w, b):
