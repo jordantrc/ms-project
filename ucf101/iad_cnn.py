815ae0322a09af1bf2f052a6e49f517ae3096a02
Jordan Chadwick <jordan.chadwick@gmail.com>
2018-12-06 12:12:25 -0500

    iad_cnn work

--- ucf101/iad_cnn.py
+++ ucf101/iad_cnn.py
@@ -6,12 +6,12 @@ import os
 import random
 import tensorflow as tf
 
-BATCH_SIZE = 1
-FILE_LIST = 'train-test-splits/testlist01.txt'
+BATCH_SIZE = 10
+FILE_LIST = 'train-test-splits/trainlist01.txt'
 MODEL_SAVE_DIR = 'iad_models/'
 LOAD_MODEL = 'iad_models/iad_model_layer_5_step_final.ckpt'
-#LOAD_MODEL = None
-EPOCHS = 1
+LOAD_MODEL = None
+EPOCHS = 20
 NUM_CLASSES = 101
 
 # neural network variables
@@ -202,8 +202,8 @@ def cnn_lenet(x, batch_size, weights, biases, dropout):
 
     # fully connected layer
     pool2_flat_size = pool2_shape[1] * pool2_shape[2] * pool2_shape[3]
-    w_fc1 = _weight_variable('W_fc1', [pool2_flat_size, 2048])
-    b_fc1 = _bias_variable('b_fc1', [2048])
+    w_fc1 = _weight_variable('W_fc1', [pool2_flat_size, 512])
+    b_fc1 = _bias_variable('b_fc1', [512])
 
     # flatten pool2
     #pool2_flat = tf.reshape(pool2, [-1, pool2_flat_size])
