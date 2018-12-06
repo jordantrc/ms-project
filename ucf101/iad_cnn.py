7a3149320ebca1bdbf41b435668df53d125edd1b
Jordan Chadwick <jordan.chadwick@gmail.com>
2018-12-06 16:11:05 -0500

    iad_cnn work

--- ucf101/iad_cnn.py
+++ ucf101/iad_cnn.py
@@ -7,11 +7,11 @@ import random
 import tensorflow as tf
 
 BATCH_SIZE = 1
-FILE_LIST = 'train-test-splits/trainlist01.txt'
+FILE_LIST = 'train-test-splits/testlist01.txt'
 MODEL_SAVE_DIR = 'iad_models/'
 LOAD_MODEL = 'iad_models/iad_model_layer_4_step_final.ckpt'
-LOAD_MODEL = None
-EPOCHS = 20
+#LOAD_MODEL = None
+EPOCHS = 1
 NUM_CLASSES = 101
 
 # neural network variables
