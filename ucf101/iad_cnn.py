396c99bc80156e0c67f4e5237b91aba56b072550
Jordan Chadwick <jordan.chadwick@gmail.com>
2018-12-06 11:16:38 -0500

    iad_cnn work

--- ucf101/iad_cnn.py
+++ ucf101/iad_cnn.py
@@ -7,7 +7,7 @@ import random
 import tensorflow as tf
 
 BATCH_SIZE = 10
-FILE_LIST = 'train-test-splits/testlist01.txt'
+FILE_LIST = 'train-test-splits/trainlist01.txt'
 MODEL_SAVE_DIR = 'iad_models/'
 #LOAD_MODEL = 'iad_models/iad_model_layer_4_step_final.ckpt'
 LOAD_MODEL = None
