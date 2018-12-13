9294a6d63ac772627eea0747217e31fd538eedf6
Jordan Chadwick <jordan.chadwick@gmail.com>
2018-12-12 22:06:36 -0500

    iad gen work

--- ucf101/c3d_model.py
+++ ucf101/c3d_model.py
@@ -521,9 +521,10 @@ def read_clip_and_label(directory, filename, batch_size, start_pos=-1, num_frame
       label.append(int(tmp_label))
 
   try:
+    print("len data = %s" % (len(data)))
     np_arr_data = np.array(data).astype(np.float32)
   except ValueError as e:
-    print("data = %s, type = %s, len = %s" % (data[0], type(data), len(data)))
+    print("data = %s, type = %s, len = %s" % (data[0][0], type(data), len(data)))
     print(e)
     sys.exit(1)
   np_arr_label = np.array(label).astype(np.int64)
