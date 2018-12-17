29b585e3c7013c9b37785e7731fe50b041f9368a
Jordan Chadwick <jordan.chadwick@gmail.com>
2018-12-17 09:06:31 -0500

    iad_cnn

--- ucf101/iad_cnn.py
+++ ucf101/iad_cnn.py
@@ -12,11 +12,11 @@ import tensorflow as tf
 import analysis
 from tfrecord_gen import CLASS_INDEX_FILE, get_class_list
 
-BATCH_SIZE = 1
-FILE_LIST = 'train-test-splits/test-iad.list'
+BATCH_SIZE = 10
+FILE_LIST = 'train-test-splits/train-iad.list'
 MODEL_SAVE_DIR = 'iad_models/'
-LOAD_MODEL = 'iad_models/iad_model_layer_1_step_final.ckpt'
-#LOAD_MODEL = None
+LOAD_MODEL = 'iad_models/iad_model_layer_2_step_final.ckpt'
+LOAD_MODEL = None
 EPOCHS = 1
 NUM_CLASSES = 101
 #CLASSES_TO_INCLUDE = ['ApplyEyeMakeup', 'Knitting', 'Lunges', 'HandStandPushups', 'Archery', 'MilitaryParade',
@@ -40,7 +40,7 @@ LEARNING_RATE = 1e-3
 # layer 4 - 512 features x 4 time slices
 # layer 5 - 512 features x 2 time slices
 FIRST_CNN_WIDTH = 32
-LAYER = 1
+LAYER = 2
 LAYER_GEOMETRY = {'1': (64, 16, 1),
                   '2': (128, 16, 1),
                   '3': (256, 8, 1),
