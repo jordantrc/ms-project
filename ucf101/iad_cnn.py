972c5119fd465de650a53b96a7bc4fda73e74e1d
Jordan Chadwick <jordan.chadwick@gmail.com>
2018-12-16 19:45:31 -0500

    iad gen work

--- ucf101/iad_cnn.py
+++ ucf101/iad_cnn.py
@@ -12,8 +12,8 @@ import tensorflow as tf
 import analysis
 from tfrecord_gen import CLASS_INDEX_FILE, get_class_list
 
-BATCH_SIZE = 1
-FILE_LIST = 'train-test-splits/test.list'
+BATCH_SIZE = 10
+FILE_LIST = 'train-test-splits/train.list'
 MODEL_SAVE_DIR = 'iad_models/'
 LOAD_MODEL = 'iad_models/iad_model_layer_1_step_final.ckpt'
 LOAD_MODEL = None
