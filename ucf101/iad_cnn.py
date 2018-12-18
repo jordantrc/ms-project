05cda719dc2644ef2bd52895975144f45dfd4905
Jordan Chadwick <jordan.chadwick@gmail.com>
2018-12-18 10:35:54 -0500

    iad_cnn

--- ucf101/iad_cnn.py
+++ ucf101/iad_cnn.py
@@ -12,11 +12,11 @@ import tensorflow as tf
 import analysis
 from tfrecord_gen import CLASS_INDEX_FILE, get_class_list
 
-BATCH_SIZE = 1
-FILE_LIST = 'train-test-splits/test-iad.list'
+BATCH_SIZE = 10
+FILE_LIST = 'train-test-splits/train-iad.list'
 MODEL_SAVE_DIR = 'iad_models/'
 LOAD_MODEL = 'iad_models/iad_model_layer_5_step_final.ckpt'
-#LOAD_MODEL = None
+LOAD_MODEL = None
 EPOCHS = 1
 NUM_CLASSES = 101
 #CLASSES_TO_INCLUDE = ['ApplyEyeMakeup', 'Knitting', 'Lunges', 'HandStandPushups', 'Archery', 'MilitaryParade',
@@ -24,7 +24,7 @@ NUM_CLASSES = 101
 #                      'BalanceBeam', 'BandMarching', 'Fencing', 'FloorGymnastics', 'Haircut', 'Hammering',
 #                      'HeadMassage', 'HighJump', 'HulaHoop', 'JavelinThrow', 'JumpingJack', 'Kayaking']
 CLASSES_TO_INCLUDE = 'all'
-TRAINING_DATA_SAMPLE = 0.5
+TRAINING_DATA_SAMPLE = 0.75
 
 # neural network variables
 WEIGHT_STDDEV = 0.1
