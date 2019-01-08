6e296151dc5ccdfd8c3acad65fa2f5411642bc80
Jordan Chadwick <jordan.chadwick@gmail.com>
2019-01-07 21:09:15 -0500

    temporal softmax

--- ucf101/iad_cnn.py
+++ ucf101/iad_cnn.py
@@ -261,16 +261,16 @@ def get_variables_temporal_softmax(model_name, num_channels=1):
     weights = {}
     biases = {}
     with tf.variable_scope(model_name) as var_scope:
-        weights['W_0a'] = _weight_variable('W_0a', [num_rows, num_rows / 2])
-        weights['W_0b'] = _weight_variable('W_0b', [num_rows, num_rows / 2])
-        weights['W_0c'] = _weight_variable('W_0c', [num_rows, num_rows / 2])
-        weights['W_0d'] = _weight_variable('W_0d', [num_rows, num_rows / 2])
-        weights['W_1'] = _weight_variable('W_1', [num_rows * 2, NUM_CLASSES])
+        weights['W_0a'] = _weight_variable('W_0a', [num_rows, num_rows])
+        weights['W_0b'] = _weight_variable('W_0b', [num_rows, num_rows])
+        weights['W_0c'] = _weight_variable('W_0c', [num_rows, num_rows])
+        weights['W_0d'] = _weight_variable('W_0d', [num_rows, num_rows])
+        weights['W_1'] = _weight_variable('W_1', [num_rows * 4, NUM_CLASSES])
 
-        biases['b_0a'] = _bias_variable('b_0a', [num_rows / 2])
-        biases['b_0b'] = _bias_variable('b_0b', [num_rows / 2])
-        biases['b_0c'] = _bias_variable('b_0c', [num_rows / 2])
-        biases['b_0d'] = _bias_variable('b_0d', [num_rows / 2])
+        biases['b_0a'] = _bias_variable('b_0a', [num_rows])
+        biases['b_0b'] = _bias_variable('b_0b', [num_rows])
+        biases['b_0c'] = _bias_variable('b_0c', [num_rows])
+        biases['b_0d'] = _bias_variable('b_0d', [num_rows])
         biases['b_1'] = _bias_variable('b_1', [NUM_CLASSES])
 
     return weights, biases
