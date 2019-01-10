61440b36373847444e10f64ab9c8ba557b09ca84
Jordan Chadwick <jordan.chadwick@gmail.com>
2019-01-10 16:30:25 -0500

    tfrecord to csv

--- ucf101/tfrecord_to_csv.py
+++ ucf101/tfrecord_to_csv.py
@@ -8,6 +8,7 @@ import csv
 import os
 import sys
 import tensorflow as tf
+import time
 
 from iad_cnn import LAYER_GEOMETRY
 
@@ -22,6 +23,14 @@ def main():
         raw = fd.read()
     lines = raw.split('\n')
     lines.remove('')
+
+    # separate into list of filenames and classes
+    filenames = []
+    classes = []
+    for l in lines:
+        f, c = l.split()
+        filenames.append(f)
+        classes.append(c)
 
     # open the output files for writing, write the header
     csv_fds = []
@@ -42,31 +51,50 @@ def main():
         csv_writer.writerow(header)
         csv_writers.append(csv_writer)
 
-    # open each file in the lines list, parse the sample
-    sess = tf.Session()
-    for l in lines:
-        f, c = l.split()
-        f_base = os.path.basename(f)
-        row = [f_base, c]
-        for example in tf.python_io.tf_record_iterator(f):
-            print("extracting data from %s" % (f))
-            features = dict()
-            features['label'] = tf.FixedLenFeature((), tf.int64, default_value=0)
 
-            for layer in range(1, 6):
-                features['img/{:02d}'.format(layer)] = tf.FixedLenFeature((), tf.string)
+    with tf.Session() as sess:
+        features = {}
+        features['label'] = tf.FixedLenFeature((), tf.int64, default_value=0)
+        for layer in range(1, 6):
+            features['img/{:02d}'.format(layer)] = tf.FixedLenFeature((), tf.string)
 
-            parsed_features = tf.parse_single_example(example, features)
+        filename_queue = tf.train.string_input_producer(filenames, num_epochs=1)
+        reader = tf.TFRecordReader()
+        _, serialized_example = reader.read(filename_queue)
 
-            with sess.as_default():
-                for layer in range(1, 6):
-                    geom = LAYER_GEOMETRY[str(layer)]
-                    frame = tf.decode_raw(parsed_features['img/{:02d}'.format(layer)], tf.float32)
-                    frame = frame.eval()
-                    frame_row = list(frame)
+        parsed_features = tf.parse_single_example(serialized_example, features_def)
+        frames = []
+        for layer in range(1, 6):
+            frames.append(tf.decode_raw(parsed_features['img/{:02d}'.format(layer)], tf.float32))
+
+        init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+        sess.run(init_op)
+
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+
+        step = 0
+        while True:
+            start = time.time()
+            try:
+                frames_output = sess.run(frames)
+                f = filenames[step]
+                c = classes[step]
+                f_base = os.path.basename(f)
+                for layer in range(5):
+                    row = [f_base, c]
+                    frame_row = frames_output[layer]
+                    frame_row = list(frame_row)
                     assert len(frame_row) == geom[0] * geom[1], "layer = %s, len(frame_row) = %s, should be %s" % (layer, len(frame_row), geom[0] * geom[1])
                     row.extend(frame_row)
-                    csv_writers[layer - 1].writerow(row)  
+                    csv_writers[layer].writerow(row)
+                end = time.time()
+                elapsed_time = end - start
+                print("[%s] read data from %s - %.03f" % (step, f, elapsed_time))
+                step += 1
+            except tf.errors.OutOfRangeError:
+                print("Completed processing tfrecord files")
+                break 
 
 
 if __name__ == "__main__":
