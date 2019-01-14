daff4b01a5398ef7806b918f7a2990935e904605
Jordan Chadwick <jordan.chadwick@gmail.com>
2019-01-14 09:28:35 -0500

    iad_cnn improvements

--- ucf101/iad_cnn.py
+++ ucf101/iad_cnn.py
@@ -643,7 +643,7 @@ if __name__ == "__main__":
 
     # get the run name
     run_name = sys.argv[1]
-    neuron_powers = list(range(1, 13))
+    neuron_powers = list(range(12, 20))
     accuracies = []
 
     for power in neuron_powers:
