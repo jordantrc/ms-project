81699308ae774163a53ab122a663b854151c9329
Jordan Chadwick <jordan@wickwick.org>
2019-01-22 21:04:15 -0500

    iad pca classifiers

--- analysis/iad_analysis.py
+++ analysis/iad_analysis.py
@@ -6,9 +6,13 @@ import os
 import sys
 
 from sklearn.decomposition import IncrementalPCA
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import AdaBoostClassifier
 
 base_dir = "/home/jordanc/datasets/UCF-101/csv/"
-pca_n_components = list(range(2, 51))
+pca_n_components = [10]
 ipca_batch_size = 1000
 knn_k = list(range(3, 20))
 
@@ -62,13 +66,28 @@ def main():
         for n in pca_n_components:
             ipca = IncrementalPCA(n_components=n, batch_size=ipca_batch_size)
             ipca.fit(train_x)
+            train_x_pca = ipca.transform(train_x)
 
             # print("ipca_components shape = %s" % str(ipca.explained_variance_ratio_.shape))
             explained_variance = [str(x) for x in ipca.explained_variance_ratio_.tolist()]
             explained_variance_str = ",".join(explained_variance)
 
             print("explained_variance,%s,%s,%s,%s" % (sample_size, layer, n, explained_variance_str))
-            sys.stdout.flush()
+
+            # knn classifier
+            classifiers = {
+                'knn': KNeighborsClassifier(3),
+                'linear_svc': SVC(kernel="linear", C=0.025),
+                'svc': SVC(gamma=2, C=1),
+                'adaboost': AdaBoostClassifier(),
+                'gaussian_nb': GaussianNB()
+                }
+
+            for k in sorted(classifiers.keys()):
+                classifier = classifiers[k]
+                classifier.fit(train_x_pca, train_y)
+                score = classifier.score(test_x, test_y)
+                print("%s = %.06f" % (k, score))
 
 
 if __name__ == "__main__":
