e3adcfe86b30746b4f4b20050e49d0ff43989eba
Jordan Chadwick <jordan.chadwick@gmail.com>
2019-02-07 14:57:30 -0500

    iad analysis work - PCA

--- ucf101/iad_gen_ucf101_norm.py
+++ ucf101/iad_gen_ucf101_norm.py
@@ -21,7 +21,7 @@ TRAIN_LIST = 'train-test-splits/train-75.list'
 TEST_LIST = 'train-test-splits/test-75.list'
 IAD_DIRECTORY = '/home/jordanc/datasets/UCF-101/iad_global_norm_32_75/'
 NPY_DIRECTORY = '/home/jordanc/datasets/UCF-101/iad_global_norm_32_75/npy/'
-TRAIN_EPOCHS = 1
+TRAIN_EPOCHS = 10
 NUM_CLASSES = 101
 # Images are cropped to (CROP_SIZE, CROP_SIZE)
 CROP_SIZE = 112
