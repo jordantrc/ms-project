3f783d62bfdff7aa24162c64b8c94bd88ad44065
Jordan Chadwick <jordan.chadwick@gmail.com>
2019-03-20 08:33:46 -0400

    c3d

--- ucf101/C3D-tensorflow-master/train_c3d_ucf101.py
+++ ucf101/C3D-tensorflow-master/train_c3d_ucf101.py
@@ -219,7 +219,8 @@ def run_training():
     for step in xrange(FLAGS.max_steps):
       start_time = time.time()
       train_images, train_labels, _, _, _, _ = input_data.read_clip_and_label(
-                      filename='list/trainlist01.txt',
+                      # filename='list/trainlist01.txt',
+                      filename='../../ucf101_all_frames/train-test-splits/trainlist01.txt',
                       batch_size=FLAGS.batch_size * gpu_num,
                       num_frames_per_clip=c3d_model.NUM_FRAMES_PER_CLIP,
                       crop_size=c3d_model.CROP_SIZE,
@@ -247,7 +248,8 @@ def run_training():
         train_writer.add_summary(summary, step)
         print('Validation Data Eval:')
         val_images, val_labels, _, _, _, _ = input_data.read_clip_and_label(
-                        filename='list/testlist01.txt',
+                        #filename='list/testlist01.txt',
+                        filename='../../ucf101_all_frames/train-test-splits/testlist01.txt',
                         batch_size=FLAGS.batch_size * gpu_num,
                         num_frames_per_clip=c3d_model.NUM_FRAMES_PER_CLIP,
                         crop_size=c3d_model.CROP_SIZE,
