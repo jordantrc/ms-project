60cbe769ca822925e013d2e0dda0a118daaf7e3b
Jordan Chadwick <jordan.chadwick@gmail.com>
2019-05-02 14:21:18 -0400

    ensemble layers

--- ucf101_ensemble/ensemble.py
+++ ucf101_ensemble/ensemble.py
@@ -24,7 +24,7 @@ use_weights = False
 aggregate_method = "average"
 
 # consensus_heuristic
-consensus_heuristic = "top_5_confidence"
+consensus_heuristic = "top_5_confidence_discounted"
 
 
 #dataset specific
@@ -99,7 +99,7 @@ def model_consensus(result):
   heuristic'''
   top_5_values = result[4].flatten()
   top_5_indices = result[5].flatten()
-
+  confidence_discount = [0.44, 0.67, 0.79, 0.84, 0.84, 1.0]
   #print("top_5_indices shape = %s" % str(top_5_indices.shape))
 
   if consensus_heuristic == 'top_5_count':
@@ -110,6 +110,13 @@ def model_consensus(result):
     confidence = [0.] * 101
     for i, v in enumerate(top_5_indices):
       confidence[v] +=  top_5_values[i]
+    consensus = np.argmax(confidence)
+
+  elif consensus_heuristic == 'top_5_confidence_discounted':
+    confidence = [0.] * 101
+    for i, v in enumerate(top_5_indices):
+      confidence_band = int(i / 6)
+      confidence[v] +=  top_5_values[i] * confidence_discount[confidence_band]
     consensus = np.argmax(confidence)
   return consensus
 
